ba17f9f9064411881285e0c0d78d3a5c4e5bf95b
Alain <alaint77@gmail.com>
2019-06-07 00:09:37 +0200

    Initial commit

---
+++ Members.js
@@ -0,0 +1,22 @@
+const members = [
+  {
+    id: 1,
+    name: "Jonh Doe",
+    email: "john@gmail.com",
+    status: "active"
+  },
+  {
+    id: 2,
+    name: "Alain Tran",
+    email: "alain@gmail.com",
+    status: "inactive"
+  },
+  {
+    id: 3,
+    name: "Michelle Hello",
+    email: "mich@gmail.com",
+    status: "active"
+  }
+];
+
+module.exports = members;
